e8aed4502580fe0ed0b612e3427a3ca873e08e43
josephsivits <josephsivits@gmail.com>
2020-10-06 00:27:33 -0500

    Before bugs

--- index.js
+++ index.js
@@ -14,7 +14,61 @@ const genres = [
     {id: 6, name:'Sports'}
 ];
 
+// getting genres api
 app.get('/api/genres',(req,res) => {
     res.send(genres);
 });
 
+// posting a new genre
+app.post('/api/genres', (req,res) => {
+    const {error} = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    const genre = {
+        id: genres.length + 1,
+        name: req.body.name
+    };
+    genres.push(genre);
+    res.send(genre);
+});
+
+// Modifying a given id
+app.put('/api/genres/:id', (req,res) => {
+    const genre = genres.find(c=>c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with this ID was not found.");
+
+    const {error} = validateGenre(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    genre.name = req.body.name;
+    res.send(genre);
+})
+
+// deleting with given id
+app.delete('/api/genres/:id',(req,res) => {
+    const genre = genres.find(c=>c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with this ID was not found.");
+
+    const index = genres.indexOf(genre);
+    genres.splice(index, 1);
+
+    res.send(genre);
+});
+
+// getting based off of an id
+app.get('/api/genres/:id', (req,res) => {
+    const genre = genres.find(c=>c.id === parseInt(req.params.id));
+    if(!genre) return res.status(404).send("The genre with this ID was not found.");
+    res.send(genre);
+});
+
+// validation using Joi
+function validateGenre(genre) {
+    const schema = {
+        name: Joi.string().min(3).required()
+    };
+    return Joi.validate(genre,schema); // 13.1.0 needed for validate([args])
+}
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Listening on port ${port}!`));
