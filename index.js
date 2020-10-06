646dec8d9ecc76cfd65cfb8b6aa75465ce89dc08
josephsivits <josephsivits@gmail.com>
2020-10-05 23:48:08 -0500

    genres

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
-
-
+const Joi = require('joi'); // input validation
 const express = require('express');
 const app = express();
+
+app.use(express.json());
+
+// Mobile Videos
+const genres = [
+    {id: 1, name:'Commentary'},
+    {id: 2, name:'Tutorials'},
+    {id: 3, name:'Comedy'},
+    {id: 4, name:'Reactions'},
+    {id: 5, name:'Educational'},
+    {id: 6, name:'Sports'}
+];
